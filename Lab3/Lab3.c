4bc8d9aadc61fd0158a7c84da79f652b4801672b
AyeshaFarkhundah <22863396+AyeshaFarkhundah@users.noreply.github.com>
2019-03-14 23:16:41 -0400

    Create Lab3.c

---
+++ Lab3/Lab3.c
@@ -0,0 +1,187 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <math.h>
+#include <stdbool.h>
+
+
+
+
+int rank=9;
+int board[rank][rank];
+int colsum=0;
+int rowsum=0;
+bool Rvalid=true;
+bool Cvalid=true;
+int num;
+int row;
+int col;
+
+
+pthread_mutex_t mutex;
+
+void *read_file(void *arg){
+	pthread_mutex_lock(&mutex);
+	FILE *fp = fopen("Input.txt","r");
+	fseek (fp, 0L, SEEK_END); //move to the end of file
+	int size = ftell(fp); //get the size
+	fseek (fp, 0, SEEK_SET); //set back to beginning of the file
+	if (size == 0) {
+		printf ("Empty file");
+	} 	else {
+		for (int i=0; i<rank; i++) {
+			for (int j=0; j<rank; j++) {
+				//empty space are marked by 0 in the file
+				fscanf(fp, "%d", &puzzle[i][j]);}
+		}
+	}
+		fclose(fp);
+		pthread_mutex_unlock(&mutex);
+}
+
+
+void *print_board(void *arg){
+	pthread_mutex_lock(&mutex);
+	for (int i=0; i<rank; i++) {
+		for (int j=0; j<rank; j++) {
+			if (board[i][j]!=0){
+			printf("%d", board[i][j]");}
+			else {
+				printf("-"); }
+			}
+			printf("\n");
+		}
+		pthread_mutex_unlock(&mutex);
+	}
+
+/* void *checkRowValid(void *arg){
+		pthread_mutex_lock(&mutex);
+
+		Rvalid=true;
+		for (int i=0; i<rank; i++) {
+			for (int j=0; j<rank; j++) {
+				if (board[i][j]!=0){
+				sumRow=board[i][j]+sumRow;
+		  	}
+				else {
+					sumRow=1+sumRow;
+				}
+				if (sumRow>9){
+					Rvalid=false;
+					break;
+				}
+
+				}
+			}
+			pthread_mutex_unlock(&mutex);
+
+	}
+
+	void *checkColValid(void *arg){
+		pthread_mutex_lock(&mutex);
+		Cvalid=true;
+
+		for (int i=0; i<rank; i++) {
+			for (int j=0; j<rank; j++) {
+				if (board[j][i]!=NULL){
+				sumCol=board[j][i]+sumCol;
+				}
+				else {
+					sumCol=1+sumCol;
+				}
+				if (sumCol>9){
+					Cvalid=false;
+					break;
+				}
+
+				}
+			}
+			pthread_mutex_unlock(&mutex);
+
+	}
+	*/
+
+
+
+	bool isInRow(int num, int row){
+		for (int j=0; j<rank; j++) {
+			if (board[row][j]==num){
+				return true;
+			}
+
+	}
+	return false
+}
+
+bool isInCol(int num, int col){
+	for (int i=0; i<rank; i++) {
+		if (board[i][col]==num){
+			return true;
+		}
+}
+return false
+}
+
+bool isInBox(int num, int rowPoint, int colPoint){
+	for (int i=0; i<3; i++) {
+		for (int j=0; j<3; j++) {
+			if (board[i+rowPoint][j+colPoint] == num) {
+				return true;
+			}
+}
+}
+return false;
+
+bool duplicates(int num,int row, int col){
+	if (isInRow(num,row)==false&& isInCol(num,col)==false&& isInBox(num,row-row%3,col-col%3)){
+		return true;
+	} else {
+		return false;
+	}
+
+}
+
+void emptySpace(void*arg){
+	for( int i=0;i<rank;i++){
+		for (int j=0;j<rank;j++){
+			if (board[i][j]=0){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool completeBoard(void*args){
+
+	if (emptySpace=false){
+		return true;
+	}
+
+	for(int num=1;num<=rank;num++){
+		if (duplicates(num,row,col)=true){
+			board[row][col]=num;
+			if (completeBoard()=true){
+				return true;
+			}
+		}
+		board[row][col]=0;
+	}
+	return false;
+}
+
+
+
+main(){
+
+	if (completeBoard()=true){
+		printf("The board is valid");
+		print_board();
+	} else {
+		printf("The board is unvalid");
+		print_board();
+	}
+
+
+}
